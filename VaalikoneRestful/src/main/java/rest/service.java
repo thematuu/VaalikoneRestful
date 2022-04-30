ac618f438d60475dc77e575a45d5313fdec72a7b
Juhhoo <jtag7@protonmail.com>
2022-04-30 22:08:18 +0300

    Lisää epäonnistumista

--- VaalikoneRestful/src/main/java/rest/service.java
+++ VaalikoneRestful/src/main/java/rest/service.java
@@ -8,6 +8,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -47,6 +48,23 @@ public class service {
 		
 		return list;
 	}
+	
+	@PUT
+	@Path("/updatefish")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public List<candidate> updateCanditate(candidate candidate) {
+		EntityManager em=emf.createEntityManager();
+		em.getTransaction().begin();
+		candidate f=em.find(candidate.class, candidate.getId()); //select * from fish where id=fish.getId()
+		if (f!=null) {
+			em.merge(candidate);//The actual update line
+		}
+		em.getTransaction().commit();
+		//Calling the method readFish() of this service
+		List<candidate> list=readCandidate();		
+		return list;
+	}	
 	
 //	@GET
 //	@Path("/getallcandidates")
